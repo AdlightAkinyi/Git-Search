671aec5101fc435b35eddfdc6e082750de8364a2
C0159412020 <adlightakinyi1@gmail.com>
2022-04-20 02:30:23 +0300

    edited file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiKey:'2248a8bd35eb376f0b586b764c50c76b67be3a1b' 
+  // apiKey:'2248a8bd35eb376f0b586b764c50c76b67be3a1b' 
+apiKey:'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n'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiKey:'2248a8bd35eb376f0b586b764c50c76b67be3a1b' 
+  // apiKey:'2248a8bd35eb376f0b586b764c50c76b67be3a1b' 
+  apiKey:'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'
 };
 
 /*
